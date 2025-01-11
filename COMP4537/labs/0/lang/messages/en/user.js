027c5abf18cd9278152c5c91dfe118401525cbfa
Navjot Kehal <navjotkehal5@gmail.com>
2025-01-11 00:44:28 -0800

    created folder structure for memory game to match the format of the requested URL

---
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -0,0 +1,6 @@
+const MIN_BOXES = 3
+const MAX_BOXES = 7
+const INITIAL_MSG = "How many buttons to create?"
+const INVALID_NUM_BOXES_MSG = "Invalid number of boxes. Number of boxes must be between " + MIN_BOXES + " and " +  MAX_BOXES + "."
+const WIN_MSG = "Excellent memory!"
+const LOSE_MSG = "Wrong order!"
