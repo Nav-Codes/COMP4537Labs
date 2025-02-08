15287ea4749244598dd8886b1ed12f0e3b931409
Navjot Kehal <navjotkehal5@gmail.com>
2025-02-08 00:26:20 -0800

    created class for dictionary read and write and rendering class that processes what is dipsayed

--- COMP4537/labs/4/js/index.js
+++ COMP4537/labs/4/js/index.js
@@ -1,103 +1,119 @@
 //to get definition of a word: https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions?word=apple
 //to write meaning for new word: https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions
 
-//will just get text saying that everything is ok after sending
-function writeNewMeaning() {
-    event.preventDefault()
-    document.getElementById("submitBtn").disabled = true
+class Dictionary {
+    static async writeNewMeaning() {
+        event.preventDefault()
+        document.getElementById("submitBtn").disabled = true
 
-    let word = document.getElementById("word").value
-    let meaning = document.getElementById("meaning").value
+        let word = document.getElementById("word").value
+        let meaning = document.getElementById("meaning").value
 
-    let obj = {
-        word : word,
-        meaning : meaning
+        let obj = {
+            word: word,
+            meaning: meaning
+        }
+
+        let jsonObj = JSON.stringify(obj)
+
+        let dictionaryObj = {}
+        //ChatGPT helped with generating the fetch request
+        await fetch("https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: jsonObj
+        })
+            .then(response => {
+                dictionaryObj.statusCode = response.status
+                return response.json()
+            })
+            .then(data => {
+                dictionaryObj.data = data
+            })
+
+        console.log(dictionaryObj)
+        DictionaryRenderer.renderWriteNewMeaning(dictionaryObj)
+
+        document.getElementById("submitBtn").disabled = false
     }
 
-    let jsonObj = JSON.stringify(obj)
+    static async getMeaning() {
+        event.preventDefault()
+        document.getElementById("submitBtn").disabled = true
 
-    let dictionaryObj = {}
-    //ChatGPT helped with generating the fetch request
-    fetch("https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions", {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        body : jsonObj
-    })
-    .then(response => {
-        // console.log(response)
-        dictionaryObj.statusCode = response.status
-        return response.json()
-    })
-    .then(data => {
-        dictionaryObj.data = data
-    })
+        let word = document.getElementById("word").value
 
-    console.log(dictionaryObj)
+        let dictionaryObj = {}
 
-    document.getElementById("submitBtn").disabled = false
-    
-    
-    //send as urlencoded
-    // let response = sendRequest("POST", "https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions", jsonObj)
+        await fetch(`https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions?word=${word}`, {
+            method: "GET"
+        })
+            .then(response => {
+                dictionaryObj.statusCode = response.status
+                return response.json()
+            })
+            .then(data => {
+                dictionaryObj.data = data
+            })
+            .catch(error => {
+                document.getElementById("submitBtn").disabled = false
+            })
+        DictionaryRenderer.renderMeaning(dictionaryObj)
+
+
+
+        //after processing is done
+        document.getElementById("submitBtn").disabled = false
+    }
 }
 
-//will get res object 
-async function getMeaning() {
-    event.preventDefault()
-    document.getElementById("submitBtn").disabled = true
+class DictionaryRenderer {
+    static renderWriteNewMeaning(dictionaryObj) {
+        switch (dictionaryObj.statusCode) {
+            case 200: {
 
-    let word = document.getElementById("word").value
+            }
+            case 400: {
 
-    let dictionaryObj = {}
+            }
+            case 404: {
 
-    await fetch(`https://king-prawn-app-7exk8.ondigitalocean.app/COMP4537/labs/4/api/definitions?word=${word}`, {
-        method: "GET"
-    })
-    .then(response => {
-        dictionaryObj.statusCode = response.status
-        return response.json()
-    })
-    .then(data => {
-        dictionaryObj.data = data
-    })
-    .catch(error => {})
-    console.log(dictionaryObj)
-    renderMeaning(dictionaryObj)
+            }
+            default: {
 
-    
-
-    //after processing is done
-    document.getElementById("submitBtn").disabled = false
-}
-
-function renderMeaning(dictionaryObj) {
-
-    switch(dictionaryObj.statusCode) {
-        case 200 : {
-            let userMsg = getMeaningMsg
-            
-            userMsg = userMsg.replace("{reqNum}", dictionaryObj.data.num)
-            userMsg = userMsg.replace("{word}", dictionaryObj.data.word)
-            userMsg = userMsg.replace("{meaning}", dictionaryObj.data.meaning)
-    
-            document.getElementById("result").innerHTML = userMsg
-            break
+            }
         }
-        case 400 : {
-            document.getElementById("result").innerHTML = improperFormatMsg
-            break
-        }
-        case 404 : { 
-            document.getElementById("result").innerHTML = wordNotFound
-            break
-        }
-        default : {
-            
+    }
+
+    static renderMeaning(dictionaryObj) {
+        switch (dictionaryObj.statusCode) {
+            case 200: {
+                let userMsg = getMeaningMsg
+
+                userMsg = userMsg.replace("{reqNum}", dictionaryObj.data.num)
+                userMsg = userMsg.replace("{word}", dictionaryObj.data.word)
+                userMsg = userMsg.replace("{meaning}", dictionaryObj.data.meaning)
+
+                document.getElementById("result").innerHTML = userMsg
+                break
+            }
+            case 400: {
+                document.getElementById("result").innerHTML = improperFormatMsg
+                break
+            }
+            case 404: {
+                let notFoundMsg = wordNotFound
+                notFoundMsg = notFoundMsg.replace("{word}", dictionaryObj.data.word)
+                document.getElementById("result").innerHTML = notFoundMsg
+                break
+            }
+            default: {
+
+            }
+
         }
 
     }
-
 }
-
--- COMP4537/labs/4/messages/lang/en/user.js
+++ COMP4537/labs/4/messages/lang/en/user.js
@@ -1,3 +1,3 @@
 const getMeaningMsg = "Request #{reqNum}<br>Word: {word}<br>Definition: {meaning}"
 const improperFormatMsg = "Input was not formatted properly. Only non-empty alphabetical strings are allowed"
-const wordNotFound = "The word %s does not exist"
+const wordNotFound = "The word \"{word}\" does not exist"
