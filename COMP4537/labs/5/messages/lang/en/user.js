76dea26aa917d299dbc5f9beedec1fde3e4b53b5
Navjot Kehal <navjotkehal5@gmail.com>
2025-02-13 19:34:18 -0800

    created get request for SELECT queries and added .toLowerCase() to checkQuery

---
+++ COMP4537/labs/5/messages/lang/en/user.js
@@ -0,0 +1,2 @@
+const dbResMsg = "Database response from server: ";
+const invalidQueryMsg = "This query is not allowed. Only SELECT and INSERT queries are allowed. Ensure your query is formatted properly."
